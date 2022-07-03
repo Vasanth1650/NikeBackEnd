cfc4606a8f1f576a9b6bfa48602ea0e4d04ab010
Vasanth1650 <vasanth16756@gmail.com>
2022-07-03 20:07:28 +0530

    dawdwa

--- src/test/java/com/example/demo/NikeShopingApplicationTests.java
+++ src/test/java/com/example/demo/NikeShopingApplicationTests.java
@@ -11,45 +11,13 @@
  * */
 
 package com.example.demo;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
 
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-import com.example.demo.dao.UserDetailsRepository;
-import com.example.demo.model.User;
-import com.example.demo.service.CustomUserService;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class NikeShopingApplicationTests {
 	
-	@Mock
-	UserDetailsRepository repositorys;
-
-	@MockBean
-	private UserDetailsRepository repository;
 	
-	@Autowired
-	private CustomUserService service;
-	
-	
-	
-	
-	@Test
-	public void getUsersTest() {
-		when(repository.findAll()).thenReturn(Stream.of(new User(1,"vasanth","vasanth123@gmail.com","raina9486","1234567890","dawda","scawca",
-				"ehiwad",true)).collect(Collectors.toList()));
-		assertEquals(1, service.getUsers().size());
-	}
 	
 
 }
